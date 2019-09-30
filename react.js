4c24e310a3ca45707eb760a5e1c7345e20d585a9
Naoya Ono <ono.naoyaa@gmail.com>
2019-10-01 00:58:23 +0800

    Add reacy config

---
+++ react.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: [
+    './',
+    'plugin:react/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['react'],
+  env: {
+    node: true,
+    browser: true,
+    es6: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/display-name': 'off',
+    'react/no-unescaped-entities': 'off',
+    'react/prop-types': ['warn', { ignore: ['children', 'className'] }],
+  },
+};
